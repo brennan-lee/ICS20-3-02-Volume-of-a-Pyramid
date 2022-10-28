6de1ec29335ad53cfe3a7b8c8c5aad65b99d0177
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-10-28 14:52:03 -0400

    index js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Brennan Lee All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Brennan Lee
+// Created on: oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,14 +10,20 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-3-01-volume-of-a-Pyramid/sw.js", {
+    scope: "/ICS20-volume-of-a-pyramid/",
   })
 }
 
-/**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function enterClicked() {
+  // input
+  const length = parseInt(document.getElementById("length-of-trapzoid").value)
+  const width = parseInt(document.getElementById("width-of-trapzoid").value)
+  const height = parseInt(document.getElementById("height-of-trapzoid").value)
+  // process
+  const volume = (length * width * height) / 3
+
+  // output
+  document.getElementById("volume-of-a-pyramid").innerHTML =
+    "the volume of the pyramid is: " + volume + " mm²."
 }
